b1017e098b6c8f594b5ebde6bbabba47599283d2
Jasmine Hassan <jasmine.aura@gmail.com>
2012-10-29 16:30:57 +0200

    [lc-p/caja-autorun] remove unused autorun code
    http://git.gnome.org/browse/nautilus/commit/?id=33df383a2692dd8d8aba802710da28a6063f3bbc

--- libcaja-private/caja-autorun.h
+++ libcaja-private/caja-autorun.h
@@ -24,15 +24,11 @@
 
 /* TODO:
  *
- * - automount all user-visible media on startup
- *  - but avoid doing autorun for these
- * - unmount all the media we've automounted on shutdown
  * - finish x-content / * types
  *  - finalize the semi-spec
  *  - add probing/sniffing code
  * - clean up code
  * - implement missing features
- *  - "Open Folder when mounted"
  *  - Autorun spec (e.g. $ROOT/.autostart)
  *
  */
@@ -44,27 +40,7 @@
 #include <eel/eel-background.h>
 #include <libcaja-private/caja-file.h>
 
-typedef void (*CajaAutorunComboBoxChanged) (gboolean selected_ask,
-        gboolean selected_ignore,
-        gboolean selected_open_folder,
-        GAppInfo *selected_app,
-        gpointer user_data);
-
-typedef void (*CajaAutorunOpenWindow) (GMount *mount, gpointer user_data);
 typedef void (*CajaAutorunGetContent) (char **content, gpointer user_data);
-
-void caja_autorun_prepare_combo_box (GtkWidget *combo_box,
-                                     const char *x_content_type,
-                                     gboolean include_ask,
-                                     gboolean include_open_with_other_app,
-                                     gboolean update_settings,
-                                     CajaAutorunComboBoxChanged changed_cb,
-                                     gpointer user_data);
-
-void caja_autorun_set_preferences (const char *x_content_type, gboolean pref_ask, gboolean pref_ignore, gboolean pref_open_folder);
-void caja_autorun_get_preferences (const char *x_content_type, gboolean *pref_ask, gboolean *pref_ignore, gboolean *pref_open_folder);
-
-void caja_autorun (GMount *mount, CajaAutorunOpenWindow open_window_func, gpointer user_data);
 
 char **caja_autorun_get_cached_x_content_types_for_mount (GMount       *mount);
 
@@ -74,8 +50,5 @@ void caja_autorun_get_x_content_types_for_mount_async (GMount *mount,
         gpointer user_data);
 
 void caja_autorun_launch_for_mount (GMount *mount, GAppInfo *app_info);
-
-void caja_allow_autorun_for_volume (GVolume *volume);
-void caja_allow_autorun_for_volume_finish (GVolume *volume);
 
 #endif /* CAJA_AUTORUN_H */
